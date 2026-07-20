614c133ed2204e573309db938d628ff33cac0e59
outamghartAchraf <outamghart2000@gmail.com>
2026-07-20 17:50:47 +0100

    add login function for return login page

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -9,4 +9,8 @@ class AuthController extends Controller
     public function register(){
         return view('auth.register');
     }
+
+    public function login(){
+        return view('auth.login');
+    }
 }
