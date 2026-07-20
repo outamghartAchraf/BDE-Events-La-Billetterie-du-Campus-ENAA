6a0d4fb6eb90d9f67579cb1007f58ca592706667
outamghartAchraf <outamghart2000@gmail.com>
2026-07-20 18:11:12 +0100

    add function registerStore

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,15 +2,41 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\RegisterRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-    public function register(){
+    public function register()
+    {
         return view('auth.register');
     }
 
-    public function login(){
+    public function login()
+    {
         return view('auth.login');
+    }
+
+    public function registerStore(RegisterRequest $request)
+{
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'role' => $request->role,
+        ]);
+
+        Auth::login($user);
+
+        if ($user->role === 'admin') {
+            return redirect('/admin/dashboard')
+                ->with('success', 'Welcome Admin!');
+        }
+
+        return redirect('/student/dashboard')
+            ->with('success', 'Welcome Student!');
     }
 }
