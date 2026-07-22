f3d5f8b1d7e15b1e12d3e3d4d4aa486ff45a986f
outamghartAchraf <outamghart2000@gmail.com>
2026-07-22 13:58:03 +0100

    create function index

--- app/Http/Controllers/Student/EventController.php
+++ app/Http/Controllers/Student/EventController.php
@@ -4,8 +4,14 @@ namespace App\Http\Controllers\Student;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Event;
 
 class EventController extends Controller
 {
-    //
+    public function index()
+    {
+        $events = Event::latest()->paginate(9);
+
+        return view('student.events.index', compact('events'));
+    }
 }
