2ba8dc59dca9663d1df1d763f9d8b1cd7b95b393
outamghartAchraf <outamghart2000@gmail.com>
2026-07-22 18:18:04 +0100

    edit profile student

--- resources/views/layouts/student.blade.php
+++ resources/views/layouts/student.blade.php
@@ -29,7 +29,7 @@
 
                 <!-- Navigation Links -->
                 <nav class="p-4 space-y-1.5">
-                    <a href=" " 
+                    <a href="{{ route('student.dashboard') }}" 
                        class="flex items-center gap-3 px-3.5 py-2.5 rounded-xl text-xs font-semibold transition-all duration-200 {{ request()->routeIs('student.dashboard') ? 'bg-indigo-50 text-indigo-600 font-bold shadow-xs' : 'text-gray-600 hover:bg-gray-50 hover:text-gray-900' }}">
                         <svg class="w-4 h-4 {{ request()->routeIs('student.dashboard') ? 'text-indigo-600' : 'text-gray-400' }}" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 00-1-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 00-1 1m-6 0h6"/></svg>
                         Dashboard
@@ -48,8 +48,8 @@
                     </a>
 
                     <a href="{{ route('student.profile') }}" 
-                       class="flex items-center gap-3 px-3.5 py-2.5 rounded-xl text-xs font-semibold transition-all duration-200 {{ request()->routeIs('student.profile.*') ? 'bg-indigo-50 text-indigo-600 font-bold shadow-xs' : 'text-gray-600 hover:bg-gray-50 hover:text-gray-900' }}">
-                        <svg class="w-4 h-4 {{ request()->routeIs('student.profile.*') ? 'text-indigo-600' : 'text-gray-400' }}" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"/></svg>
+                       class="flex items-center gap-3 px-3.5 py-2.5 rounded-xl text-xs font-semibold transition-all duration-200 {{ request()->routeIs('student.profile') ? 'bg-indigo-50 text-indigo-600 font-bold shadow-xs' : 'text-gray-600 hover:bg-gray-50 hover:text-gray-900' }}">
+                        <svg class="w-4 h-4 {{ request()->routeIs('student.profile') ? 'text-indigo-600' : 'text-gray-400' }}" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"/></svg>
                         Profile
                     </a>
                 </nav>
